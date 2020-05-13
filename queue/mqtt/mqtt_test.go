353e609f7e73663c8c195b01f4ea51beaac2eaf7
toniz <toniz7960@gmail.com>
2020-05-13 16:41:50 +0800

    modify mqtt.test

--- queue/mqtt/mqtt_test.go
+++ queue/mqtt/mqtt_test.go
@@ -5,48 +5,61 @@
 package mqtt_test
 
 import (
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-	"log"
+    "log"
+    . "github.com/onsi/ginkgo"
+    . "github.com/onsi/gomega"
+    "fmt"
 
-	. "github.com/toniz/gosuit/queue"
-	_ "github.com/toniz/gosuit/queue/mqtt"
+    . "github.com/toniz/gosuit/queue"
+    _ "github.com/toniz/gosuit/queue/mqtt"
 )
 
 var _ = Describe("Matt Test", func() {
 
-	mq, _ := NewMessageQueue("mqtt")
-	Context("Test Connection", func() {
-		endpoint := "tcp://127.0.0.1:1883"
+    mq, _ := NewMessageQueue("mqtt")
+    Context("Test Connection", func() {
+        endpoint := "tcp://127.0.0.1:1883"
 
-		It("Should Return No Error", func() {
-			err := mq.Connect(endpoint, "", "")
-			Expect(err).NotTo(HaveOccurred())
-		})
-	})
+        It("Should Return No Error", func() {
+            err := mq.Connect(endpoint, "", "")
+            Expect(err).NotTo(HaveOccurred())
+        })
+    })
 
-	Describe("Test Mqtt API", func() {
-		Context("Test Worker", func() {
-			queueName := "test/task_queue"
-			It("Should Return No Error", func() {
-				err := mq.Worker(queueName, func(s []byte) int {
-					log.Printf("Received a message: %s.", s)
-					return 0
-				})
+    choke := make(chan string)
+    Describe("Test Mqtt API", func() {
+        Context("Test Worker", func() {
+            queueName := "test/task_queue"
+            It("Should Return No Error", func() {
+                err := mq.Worker(queueName, func(s []byte) int{
+                    log.Printf("Received a message: %s.", s)
+                    choke <- fmt.Sprintln(s)
+                    return 0
+                })
 
-				Expect(err).NotTo(HaveOccurred())
-			})
-		})
+                Expect(err).NotTo(HaveOccurred())
+            })
+        })
 
-		Context("Test New Task", func() {
-			queueName := "test/task_queue"
-			msg := "Test Mqtt Task Send"
+        Context("Test New Task", func() {
+            queueName := "test/task_queue"
+            msg := "Test Mqtt Task Send"
 
-			It("Should Return No Error", func() {
-				log.Println("Send a message")
-				err := mq.SendTask(queueName, msg)
-				Expect(err).NotTo(HaveOccurred())
-			})
-		})
-	})
+            It("Should Return No Error", func() {
+                log.Println("Send a message")
+                err := mq.SendTask(queueName, msg)
+                Expect(err).NotTo(HaveOccurred())
+                err = mq.SendTask(queueName, msg)
+                Expect(err).NotTo(HaveOccurred())
+                receiveCount := 0
+                for receiveCount < 2 {
+                    incoming := <- choke
+                    fmt.Printf("RECEIVED MESSAGE: %s\n", incoming)
+                    receiveCount++
+                }
+            })
+        })
+    })
+
+
 })
