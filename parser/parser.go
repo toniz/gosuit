07fa22c444b5848aca115a07be9225088c41e707
toniz <toniz7960@gmail.com>
2020-08-24 11:08:15 +0800

    parse add glog

--- parser/parser.go
+++ parser/parser.go
@@ -7,6 +7,7 @@ package parser
 
 import (
     "errors"
+    glog "github.com/golang/glog"
 
     "strings"
     "encoding/json"
@@ -22,20 +23,30 @@ func Decode(name string, text []byte, l interface{}) error {
         t = strings.Replace(t, "\n", " ", -1)
         t = strings.Replace(t, "\r", " ", -1)
         err := json.Unmarshal([]byte(t), l)
+        if err != nil {
+            glog.Warningf("Decode Parse Failed[%v]: %v", err, t)
+        }
         return err
     }
     case "xml", ".xml":
     {
         err := xml.Unmarshal(text, l)
+        if err != nil {
+            glog.Warningf("Decode Parse Failed[%v]: %v", err, text)
+        }
         return err
     }
     case "yaml", ".yaml":
     {
         err := yaml.Unmarshal(text, l)
+        if err != nil {
+            glog.Warningf("Decode Parse Failed[%v]: %v", err, text)
+        }
         return err
     }
     default:
     {
+        glog.Warningf("Decode Parse No Decoder Being Add")
         return errors.New("No Decoder Being Add.")
     }
     }
